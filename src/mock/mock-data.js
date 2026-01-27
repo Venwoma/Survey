2434263397369cf220ff8dc3dbe1c66489ddffcc
wei.luo <449023948@example.com>
2026-01-27 16:04:35 +0800

    feat: 动态看板制作

--- src/mock/mock-data.js
+++ src/mock/mock-data.js
@@ -1,7 +1,9 @@
 export default {
     'POST /api/auth/jwt/login': {
-        'code|1': [200, 403],
-        message: 'Login successful',
+        'code|1': [200, 403, 502],
+        message: function () {
+            return this.code === 200 ? 'Login successful' : this.code === 403 ? 'Login error' : 'server is error';
+        },
         data: {
             token: 'mock_jwt_token_123456789',
             user: {
